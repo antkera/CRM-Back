71b3656d1cd85848c336e1224e343f91067f9c9d
antkera <antkera@gmail.com>
2024-09-08 22:32:22 +0200

    listenig to 0.0.0.0

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
+app.listen(PORT,`0.0.0.0`, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
